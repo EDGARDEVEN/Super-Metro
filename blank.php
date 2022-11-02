9c33481cf121c646ae8492f404d08269c99ba63d
Edgar Deven <54006845+EDGARDEVEN@users.noreply.github.com>
2022-11-02 12:29:00 +0300

    Update blank.php
    ADMIN PAGE

--- blank.php
+++ blank.php
@@ -9,7 +9,7 @@
     <meta name="description" content="">
     <meta name="author" content="">
 
-    <title>SB Admin - Blank Page</title>
+    <title>SUPER METRO ADMIN PAGE</title>
 
     <!-- Bootstrap core CSS-->
     <link href="vendor/bootstrap/css/bootstrap.min.css" rel="stylesheet">
